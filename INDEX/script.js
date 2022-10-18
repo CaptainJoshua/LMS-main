0cfff3c923a1f01cddaf29168d5585be76e14de8
CaptainJoshua <103025478+CaptainJoshua@users.noreply.github.com>
2022-10-18 20:07:40 +0800

    trial1

--- INDEX/script.js
+++ INDEX/script.js
@@ -33,3 +33,4 @@ sidebarToggle.addEventListener("click", () => {
 console.log("Hello World"); // Test 1
 console.log("Hello World"); // Test 2
 console.log("Hello World"); // Test 3 by @Joshua453210
+console.log("Hello World"); // Test 4 by @Joshua453210
